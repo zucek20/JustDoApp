2198565ba4c968f3d67f02cd6837581568efd6df
Paweł Izdebski <p.izdebski14@gmail.com>
2022-12-29 19:32:43 +0100

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { GeneratorComponent } from './generator/generator.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    GeneratorComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/assets/quoteList.ts
@@ -0,0 +1,16 @@
+import { quote } from "src/models/quote";
+
+export const quoteList: quote[] = [
+  {
+    quote: 'Nie pozwól, by twoje marzenia zostały marzeniami',
+    author: 'Shia le bauf'
+  },
+  {
+    quote: '2',
+    author: '2'
+  },
+  {
+    quote: '3',
+    author: '3'
+  }
+]
